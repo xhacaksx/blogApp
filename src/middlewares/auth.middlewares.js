b0f9a904c3ee6a6db2bbceeb2df452982bb5526c
xhacaksx <smartakshat007@gmail.com>
2023-12-21 16:36:09 +0530

    Bug fix

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.models.js";
 import jwt from "jsonwebtoken";
 
-export const verifJWT = asyncHandler(async (req, res, next) => {
+export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
