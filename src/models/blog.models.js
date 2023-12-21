c1950b2f216047e1373f3ead500ea91475c6803c
xhacaksx <smartakshat007@gmail.com>
2023-12-21 20:09:19 +0530

    Add blog model

---
+++ src/models/blog.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    // coverImageURL: {
+    //   type: String, //url of third party service
+    //   //required: false,
+    // },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Blog = mongoose.model("Blog", blogSchema);
