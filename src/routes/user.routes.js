25e83aa30c7876444dc5e3ed1ba5d3fba79fc673
xhacaksx <smartakshat007@gmail.com>
2023-12-21 16:38:41 +0530

    Add user routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
+const router = Router();
+
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+
+export default router;
